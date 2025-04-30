98667372482223822f9985930da36cee3e2263a5
PetJs <peterfagoroye76@gmail.com>
2025-04-30 18:14:51 +0100

    style: Adds style

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 
 
 
 const App = () => {
   return(
-    <div>
+    <main className="max-w-7xl max-auto">
       <Navbar/>
-    </div>
+      <Hero/>
+    </main>
   )
 
 }
