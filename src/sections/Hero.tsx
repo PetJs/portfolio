a300bd4cec975f915cca4b7e573628aa902743e0
PetJs <peterfagoroye76@gmail.com>
2025-05-01 14:31:10 +0100

    feat: :sparkles: Updates Hero Section to include Components for 3Js features

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,60 @@
 import CanvasLoader from "@/components/CanvasLoader";
+import Cube from "@/components/Cube";
 import HackerRoom from "@/components/HackerRoom";
+import ReactLogo from "@/components/ReactLogo";
+import Rings from "@/components/Rings";
+import Target from "@/components/Target";
+import { calculateSizes } from "@/constants";
 import { PerspectiveCamera } from "@react-three/drei";
 import {Canvas} from "@react-three/fiber"
+// import { Leva, useControls } from "leva";
 import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = ()=>{
+    // const x = useControls('HackerRoom', {
+    //     positionX: {
+    //         value: 2.5,
+    //         min: -10,
+    //         max: 10,
+    //     },
+    //     positionY: {
+    //         value: 2.5,
+    //         min: -10,
+    //         max: 10,
+    //     },
+    //     positionZ: {
+    //         value: 2.5,
+    //         min: -10,
+    //         max: 10,
+    //     },
+    //     rotationX: {
+    //         value: 0,
+    //         min: -10,
+    //         max: 10
+    //     },
+    //     rotationY: {
+    //         value: 0,
+    //         min: -10,
+    //         max: 10
+    //     },
+    //     rotationZ: {
+    //         value: 0,
+    //         min: -10,
+    //         max: 10
+    //     },
+    //     scale: {
+    //         value: 1,
+    //         min: 0.1,
+    //         max: 10,
+    //     }
+    // } )
+
+    const isSmall = useMediaQuery({maxWidth: 440})
+    const isMobile = useMediaQuery({maxWidth: 768})
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024})
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet)
 
     return(
         <section className="min-h-screen w-full flex flex-col relative">
@@ -14,10 +64,25 @@ const Hero = ()=>{
             </div>
 
             <div className="w-full h-full absolute inset-0">
+                {/* <Leva/> */}
                 <Canvas className="w-full h-full">
                     <Suspense fallback={<CanvasLoader/>}>
-                        <PerspectiveCamera makeDefault position={[0, 0, 30]}/>
-                        <HackerRoom scale={0.07} position={[0, 0, 0]} wrotation={[0, -Math.PI / 2, 0]} />
+                        <PerspectiveCamera makeDefault position={[0, 0, 20]}/>
+                        <HackerRoom 
+                            // scale={0.07} 
+                            // position={[0, 0, 0]} 
+                            // rotation={[0, -Math.PI / 2, 0]}
+                            position={sizes.deskPosition}
+                            rotation={[0, -Math.PI, 0]}
+                            scale={sizes.deskScale}
+                        />
+                        <group>
+                            <Target position={sizes.targetPosition}/>
+                            <ReactLogo position={sizes.reactLogoPosition}/>
+                            <Cube position={sizes.cubePosition}/>
+                            <Rings position={sizes.ringPosition}/>
+                        </group>
+                        
                         <ambientLight intensity={1}/>
                         <directionalLight position={[10,10,10]}  intensity={1.5}/>
                     </Suspense>
