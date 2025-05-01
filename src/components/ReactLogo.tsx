da587dbead7f477527ce4b8e3c95eb739c700475
PetJs <peterfagoroye76@gmail.com>
2025-05-01 14:29:28 +0100

    feat: :sparkles: Adds ReactLogo components

---
+++ src/components/ReactLogo.tsx
@@ -0,0 +1,25 @@
+import { Float, useGLTF } from '@react-three/drei'
+import * as THREE from "three";
+
+const ReactLogo = (props: any) => {
+  const { nodes, materials } = useGLTF('/models/react.glb')
+  return (
+    <Float floatIntensity={1} >
+      <group position={[8, 8,0]} dispose={null} scale={0.3} {...props}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={(nodes['React-Logo_Material002_0'] as THREE.Mesh).geometry}
+          material={materials['Material.002']}
+          position={[0, 0.079, 0.181]}
+          rotation={[0, 0, -Math.PI / 2]}
+          scale={[0.39, 0.39, 0.5]}
+        />
+      </group>
+    </Float>
+  )
+}
+
+useGLTF.preload('/models/react.glb')
+
+export default ReactLogo;
