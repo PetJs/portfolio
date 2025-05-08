619b52d06110f5d5c7e6a01936564c331518d067
PetJs <peterfagoroye76@gmail.com>
2025-05-08 12:07:00 +0100

    feat: :sparkles: Add webpage icon

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -177,7 +177,7 @@ export const calculateSizes = (isSmall: any, isMobile: any, isTablet: any) => {
       deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
       cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
       reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
-      ringPosition: isSmall ? [-5, 10, 0] : isMobile ? [-24, 10, 0] : isTablet ? [-16, 10, 0] : [-24, 10, 0],
+      ringPosition: isSmall ? [-5, 10, 0] : isMobile ? [-12, 10, 0] : isTablet ? [-12, 10, 0] : [-12, 10, 0],
       targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
     };
 };
